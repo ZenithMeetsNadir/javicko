0e5e83cb75cb1808ec5d5b645bc71cc0695beea8
ZenithMeetsNadir <martin-vlcek@post.cz>
2025-01-21 11:30:29 +0100

    init commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import javax.swing.*;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("window");
+        frame.setSize(300, 300);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+}
