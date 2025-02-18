e5529d56e3e679d0271a481e720ec25b8599e604
ZenithMeetsNadir <martin-vlcek@post.cz>
2025-02-18 11:29:04 +0100

    customMath methods

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -21,6 +21,8 @@ public class Main {
         server.start();
 
         System.out.println("Server started on port 8080");
+        System.out.println(CustomMath.PI(1000000));
+        System.out.println(CustomMath.greatestFactor(2048, 768));
     }
 
     private static Map<String, String> queryToMap(String query) {
@@ -101,6 +103,27 @@ public class Main {
             OutputStream os = exchange.getResponseBody();
             os.write(response.getBytes());
             os.close();
+        }
+    }
+
+    private static final class CustomMath {
+        public static double PI(long iterations) {
+            double result = 0;
+
+            for (long i = 1; i <= iterations; i++) {
+                result += 4.0 / (i * 2 - 1) * (i % 2 == 1 ? 1 : -1);
+            }
+
+            return result;
+        }
+
+        public static long greatestFactor(long a, long b) {
+            if (a == b)
+                return a;
+            else if (a > b)
+                return greatestFactor(a - b, b);
+            else
+                return greatestFactor(a, b - a);
         }
     }
 }
