d8078c8cf273df0fece651224216bdcb027fc881
ZenithMeetsNadir <martin-vlcek@post.cz>
2025-03-18 11:27:13 +0100

    bubble sort

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
         int[] arr = {11100, -55050, 84940, 848480, 45, 88, -9874, 67676, 55555, 10, 1, 0, -9};
+        int[] arr3 = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+        bubbleSort(arr3);
+        //sort(arr3);
+        for (int i : arr3) {
+            System.out.print(i + "; ");
+        }
+        System.out.println();
 
         for (int[] arrayPart : split(arr, 5)) {
             for (int i : arrayPart) {
@@ -57,5 +64,32 @@ public class Main {
         System.arraycopy(array, rightStart, result[1], 0, array.length - rightStart);
 
         return result;
+    }
+
+    private static void swap(int[] pair, int index) {
+        int temp = pair[index];
+        pair[index] = pair[index + 1];
+        pair[index + 1] = temp;
+    }
+
+    private static void bubbleSort(int[] array) {
+        boolean isSorted = false;
+        int complexityPoints = 0;
+        int n = 0;
+        while (!isSorted) {
+            isSorted = true;
+            for (int i = 0; i < array.length - n - 1; i++) {
+                complexityPoints++;
+                if (array[i] > array[i + 1]) {
+                    swap(array, i);
+                    if (i > 0 && array[i - 1] > array[i])
+                        isSorted = false;
+                }
+            }
+
+            n++;
+        }
+
+        System.out.println("Complexity points: " + complexityPoints);
     }
 }
